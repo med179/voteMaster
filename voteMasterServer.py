269a008aded53b413244a197c0194d58893a3a97
med179 <med179@rambler.ru>
2018-08-01 16:34:08 +0300

    new ver

--- voteMasterServer.py
+++ voteMasterServer.py
@@ -6,16 +6,51 @@
 
 #список стран пользователей
 countries = ['riba_kit', 'tridevCarstvo', 'lukomore', 'morskayaDergava', 'shahman']
-question = ['test', 'Отказ от серебряно-золотого международного валютного стандарта', 'Использование территории Чудо-юдо рыбы Кита для размещения коалиционного флота', 'Приостановление членства в Организации Объединенных сказочных Наций Кощеева царства', 'Введение эмбарго на мертвую воду для Кощеева царства', 'Создание бесполетной зоны над Кощеевым царством']
+question = ['Отказ от серебряно-золотого международного валютного стандарта', 'Использование территории Чудо-юдо рыбы Кита для размещения коалиционного флота', 'Приостановление членства в Организации Объединенных сказочных Наций Кощеева царства', 'Введение эмбарго на мертвую воду для Кощеева царства', 'Создание бесполетной зоны над Кощеевым царством']
 from bottle import route, run, template
 
-@route('/authorization/<name>')
-def index(name):
-    if name == 'riba_kit':
-        return question  #+ '<b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
-    if name == 'tridevCarstvo':
-        return template('<b>Hello {{name}}</b>!', name=name)
-    else:
-        return template('<b>Hello {{name}}</b>!', name=name)
+@route('/authorization/<round>/<name>')
+def index(round, name):
+    if round == 'one':
+        i = 0
+        if name == 'riba_kit':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
+        if name == 'tridevCarstvo':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ПРОТИВ</b>'
+        else:
+            return question[i]
+    if round == 'two':
+        i = 1
+        if name == 'tridevCarstvo':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
+        if name == 'riba_kit':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ПРОТИВ</b>'
+        else:
+            return question[i]
+    if round == 'three':
+        i = 2
+        if name == 'lukomore':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
+        if name == 'morskayaDergava':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ПРОТИВ</b>'
+        else:
+            return question[i]
+    if round == 'four':
+        i = 3
+        if name == 'shahman':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
+        if name == 'lukomore':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ПРОТИВ</b>'
+        else:
+            return question[i]
+    if round == 'five':
+        i = 4
+        if name == 'morskayaDergava':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ЗА</b>'
+        if name == 'shahman':
+            return question[i]  + '<br /> <b>ЗА ЭТОТ ВОПРОС БОЛЬШИНСТВО ДОЛЖНЫ ПРОГОЛОСОВАТЬ ПРОТИВ</b>'
+        else:
+            return question[i]
+
 
 run(host='localhost', port=8080)
